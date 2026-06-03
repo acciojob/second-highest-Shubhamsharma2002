27bca549358eb602d391ae98629911d03739fb62
Shubham Sharma <142394297+Shubhamsharma2002@users.noreply.github.com>
2026-06-03 17:34:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+  let first = -Infinity;
+  let second = -Infinity;
+
+  for (let num of arr) {
+    if (num > first) {
+      second = first;
+      first = num;
+    } else if (num > second && num !== first) {
+      second = num;
+    }
+  }
+
+  return second;
+}
