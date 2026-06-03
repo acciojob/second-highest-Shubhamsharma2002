46dbde15d52a6392659f918d4f728432bc273c8b
Shubham Sharma <142394297+Shubhamsharma2002@users.noreply.github.com>
2026-06-03 17:39:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
 function secondHighest(arr) {
-  let first = -Infinity;
-  let second = -Infinity;
+    let first = -Infinity;
+    let second = -Infinity;
 
-  for (let num of arr) {
-    if (num > first) {
-      second = first;
-      first = num;
-    } else if (num > second && num !== first) {
-      second = num;
+    for (let num of arr) {
+        num = Number(num);
+
+        if (num > first) {
+            second = first;
+            first = num;
+        } else if (num > second && num !== first) {
+            second = num;
+        }
     }
-  }
 
-  return second;
+    return second;
 }
-
-let n = Number(prompt());
-let arr = [];
-
-for (let i = 0; i < n; i++) {
-  arr.push(Number(prompt()));
-}
-
-alert(secondHighest(arr));
